6d7b20893d6c59fc4a7143f189305d0d8d700ac1
Jesus G. Priego <70105185+JesusGPriego@users.noreply.github.com>
2023-07-11 19:28:21 +0200

    functionallity update

--- src/components/favorites/FavoriteCard.tsx
+++ src/components/favorites/FavoriteCard.tsx
@@ -1,12 +1,19 @@
 import React, { FC } from 'react';
 import { Grid, Card } from '@nextui-org/react';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 interface Props {
   id: number;
 }
 
 export const FavoriteCard: FC<Props> = ({ id }) => {
+  const router = useRouter();
+
+  const onPress = () => {
+    router.push(`/pokemon/${id}`);
+  };
+
   return (
     <Grid xs={6} sm={3} md={2} xl={1}>
       <Card
@@ -15,6 +22,7 @@ export const FavoriteCard: FC<Props> = ({ id }) => {
         css={{
           padding: 10,
         }}
+        onPress={onPress}
       >
         <div
           style={{
